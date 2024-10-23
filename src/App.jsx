e5586a2154648bf20cdb255603990a50af59649f
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2024-10-23 14:04:01 +0530

    added login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
-
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
